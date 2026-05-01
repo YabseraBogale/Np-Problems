b403da105a238901695cd569b331f87f6dd14954
YabseraBogale <yabserapython@gmail.com>
2026-05-01 21:15:07 +0300

    leetcode

--- two_sum/Program.cs
+++ two_sum/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO.MemoryMappedFiles;
+using System.Runtime;
+
+int[] num={3,2,4};
+int target=6;
+
+int[] TwoSums(int[] nums,int target)
+{
+   Dictionary<int,int> map=new Dictionary<int,int>();
+   for(int i = 0; i < nums.Length; i++)
+    {
+        int complement=target-nums[i];
+        Console.WriteLine(complement);
+        if (map.ContainsKey(complement))
+        {
+            return new int[] {map[complement],i};
+        }
+        map[nums[i]]=i;
+    } 
+    return new int[]{};
+}
+
+
