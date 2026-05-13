9174ea5ac0c7388db8ce554d16f39c00ca2a0dc0
YabseraBogale <yabserapython@gmail.com>
2026-05-13 18:02:56 +0300

    making derivatives type

--- numerical_analysis/algorthims/algorthims.go
+++ numerical_analysis/algorthims/algorthims.go
@@ -1,3 +1,24 @@
 package algorthims
 
-type RealFunction func(float64) float64
+import (
+	"fmt"
+	"math"
+)
+
+func NewtonRaphson(f func(float64) float64, fd func(float64) float64, initialGuess float64, tolerance float64, maxIterations int) (float64, error) {
+	x := initialGuess
+	for i := 0; i < maxIterations; i++ {
+		fx := f(x)
+		dfx := fd(x)
+		if math.Abs(dfx) < 1e-12 {
+			return x, fmt.Errorf("derivative too small; possible local extremum at x = %s", x)
+		}
+		nextX := x - (fx / dfx)
+		if math.Abs(nextX-x) < tolerance {
+			fmt.Printf("Converged in %d iterations.\n", i+1)
+			return nextX, nil
+		}
+		x = nextX
+	}
+	return x, nil
+}
