cf51d7751c3e06d12c935afec11ce7b2c70c91a9
YabseraBogale <yabserapython@gmail.com>
2026-05-22 19:12:55 +0300

    Jaccard index

--- numerical_analysis/algorthims/algorthims.go
+++ numerical_analysis/algorthims/algorthims.go
@@ -18,6 +18,10 @@ func HammingDistance(string_1, string_2 string) (int, error) {
 	return count, nil
 }
 
+func JaccardIndex() (float64, error) {
+	return 0, nil
+}
+
 func NewtonRaphson(f func(float64) float64, fd func(float64) float64, initialGuess float64, tolerance float64, maxIterations int) (float64, error) {
 	x := initialGuess
 	for i := 0; i < maxIterations; i++ {
